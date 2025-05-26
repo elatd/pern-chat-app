b0b541cd36ed89740b1f255433678dfddc010550
elatd <84830972+elatd@users.noreply.github.com>
2025-05-26 10:30:34 +0900

    Fix server startup and Prisma import errors

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [2025-04-10 12:00 UTC]
+**File Updated:** server/db/prisma.ts
+- Modified PrismaClient import to support CommonJS module
+- Added file header documentation
+
+**File Created:** CHANGELOG.md
+- Initial creation of changelog file
--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -1,4 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+/*
+ File: server/db/prisma.ts
+ Created: Prior to 2025-04-10
+ Last Modified: 2025-04-10 12:00 UTC
+ Purpose: Initialize and export Prisma client instance
+ Changes:
+ - 2025-04-10 12:00 UTC: Modified import statement to support CommonJS module
+*/
+
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const prisma = new PrismaClient({});
 
